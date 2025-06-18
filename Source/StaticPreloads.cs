a5d68fb74a62ce62677c00e20cfcba8d74fd8193
Jakob Hellermann <jakob.hellermann@protonmail.com>
2025-06-18 09:33:57 +0200

    Hollow Knight Example Mod

---
+++ Source/StaticPreloads.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using Modding;
+
+namespace StaticPreloadExample;
+
+public class StaticPreloadMod : Mod, ITogglableMod {
+    public static StaticPreloadMod? LoadedInstance { get; private set; }
+
+    public override string GetVersion() => Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+    public override void Initialize() {
+        if (LoadedInstance != null) return;
+        LoadedInstance = this;
+    }
+
+    public void Unload() {
+        LoadedInstance = null;
+    }
+}
